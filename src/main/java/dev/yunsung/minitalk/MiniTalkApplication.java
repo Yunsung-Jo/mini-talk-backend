1ee2edbda29e25a260f3c44c13423ffe0c21487c
조윤성 <yunsung1206@gmail.com>
2024-05-26 18:59:48 +0900

    Initial commit

---
+++ src/main/java/dev/yunsung/minitalk/MiniTalkApplication.java
@@ -0,0 +1,13 @@
+package dev.yunsung.minitalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/yunsung/minitalk/MiniTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yunsung.minitalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
